bb5f4278cfddaefaa51024f876a54b9aaddc594a
sebastiansson <sebastian.marcusson@gmail.com>
2019-05-13 16:28:35 +0200

    Return null on empty file field

--- web/app/themes/ourway/app/setup.php
+++ web/app/themes/ourway/app/setup.php
@@ -330,3 +330,11 @@ add_filter( 'rest_prepare_user', function( $response, $user, $request ) {
     return $response;
 
 }, 10, 3 );
+
+add_filter('acf/format_value/type=file', function($value, $post_id, $field) {
+    if (empty($value)) {
+        return null;
+    }
+
+    return $value;
+}, 100, 3);
